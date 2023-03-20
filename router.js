7b3245d0c9454ad56287ad0047fd22f2766b2a5d
Eder Queiroz <62737156+Eder-Queiroz@users.noreply.github.com>
2023-03-19 23:37:57 -0300

    Craido CRUD User

--- router.js
+++ router.js
@@ -5,5 +5,7 @@ import UserController from './src/controllers/User/UserController.js';
 
 // USERS
 routes.post('/new-user', UserController.insert);
+routes.put('/update-user/:id', UserController.update);
+routes.delete('/delete-user', UserController.delete);
 
 export default routes;
--- src/controllers/User/UserController.js
+++ src/controllers/User/UserController.js
@@ -52,4 +52,97 @@ export default class UserController {
 
     }
 
+    static async update(req, res) {
+
+        let id = req.params.id;
+
+        const password = await bcrypt.hash(req.body.password, 10);
+        req.body.password = password;
+
+        let status;
+
+        let response = await UserController.updateDatabase(req.body, id);
+
+        if(response.error) {
+
+            status = 400;
+            console.error(`Error ao atualizar Usuario! Req: ${JSON.stringify(req.body)}, Res:${response.response}`);
+
+        } else {
+
+            status = 200;
+            console.log(`Usuario atualizado com sucesso! Req: ${JSON.stringify(req.body)}, Res:${response.response}`);
+
+        }
+
+        return res.status(status).json(response);
+
+    }
+
+    // todo - atualizar a query do update
+
+    static async updateDatabase(body, id) {
+
+        return new Promise(async (resolve, reject) => {
+
+            let response;
+
+            User.update(body, {where: {id: id}}).then(() => {
+                response = new BasicAPIResponse('Usuario atualizado com sucesso!', false);
+                resolve(response);
+            }).catch((error) => {
+                response = new BasicAPIResponse('Falha ao  atualizar usuario!', true);
+                resolve(response);
+            });
+
+        });
+
+    }
+
+    static async delete(req, res) {
+
+        let id = req.body.id;
+
+        let response = UserController.deleteDatabase(id);
+
+        let status;
+
+        if(response.response) {
+
+            status = 400;
+            console.error(`Error ao deletear Usuario! Req: ${JSON.stringify(req.body)}, Res:${response.response}`);
+
+        } else {
+
+            status = 200;
+            console.log(`Usuario deletado com sucesso! Req: ${JSON.stringify(req.body)}, Res:${response.response}`);
+
+        }
+
+        return res.status(status).json(response);
+        
+    }
+
+    static async deleteDatabase(id) {
+        
+        return new Promise(async (resolve, reject) => {
+
+            let response;
+
+            User.update({is_deleted: true, deleted_dateTime: new Date()}, {where: {id: id}}). then(() => {
+
+                response = new BasicAPIResponse('Usuario deletado com sucesso!', false);
+                resolve(response);
+
+            }).catch((error) => {
+
+                response = new BasicAPIResponse(`Falha ao deletar usuario: ${error}`, true);
+                resolve(response);
+
+            });
+
+        });
+
+    }
+
 }
